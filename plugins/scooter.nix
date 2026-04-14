c5022488f2b4b12f67c553c8e92cb3667df0a359
Isaac Corbrey <isaac@isaaccorbrey.com>
2026-04-14 15:21:26 -0400

    [wip] plugins: Add thomasschafer/scooter.hx

---
+++ plugins/scooter.nix
@@ -0,0 +1,14 @@
+{ mkPlugin }: mkPlugin {
+  name = "scooter.hx";
+  description = "Interactive find-and-replace Helix plugin";
+
+  src = fetchGit {
+    url = "https://github.com/thomasschafer/scooter.hx";
+    rev = "eaf2de26eed45e1405df72d22a6400709870802a";
+    ref = "main";
+  };
+
+  main = "scooter.scm";
+
+  version = "0.1.4";
+}
